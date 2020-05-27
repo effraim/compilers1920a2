5f74ad6ad616adf4dfcfde877c87d95a8913b03a
effraim <32572199+effraim@users.noreply.github.com>
2020-05-27 11:38:21 +0300

    Update html-processor.py
    μηχανή ταιριάσματος 2ου ζητούμενου

--- html-processor.py
+++ html-processor.py
@@ -4,7 +4,10 @@ with open("testpage.txt","r") as fp:
   text = fp.read()
   #1. εξαγωγή και εκτύπωση του τίτλου
   
-  #μηχανή ταιριάσματος
   rexp = re.compile(r"<title>(.+?)</title>")
   m = repx.search(text)
   printf(m.group(1))
+  
+  #2. απαλοιφή σχολίων
+  repx2 = re.compile(r"<!--.*?-->",re.DOTALL)
+  
